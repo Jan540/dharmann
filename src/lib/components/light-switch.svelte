505f7eac75cfdc212d449d9e66e2e0ee1a869073
Jan540 <naj.schaefer@gmail.com>
2024-02-11 20:18:03 +0100

    light switch + new theme

---
+++ src/lib/components/light-switch.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Moon, Sun } from 'lucide-svelte';
+	import { mode, toggleMode } from 'mode-watcher';
+</script>
+
+<!-- TODO: use bits-ui -->
+
+<button
+	on:click={toggleMode}
+	class="fixed bottom-4 right-4 rounded-lg bg-background p-3 outline-none ring-1 ring-border transition-all hover:bg-border md:p-4"
+>
+	{#if $mode === 'dark'}
+		<Sun class="h-[1.4rem] w-[1.4rem] transition-all" />
+	{:else}
+		<Moon
+			class="h-[1.4rem] w-[1.4rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+		/>
+	{/if}
+	<span class="sr-only">Toggle theme</span>
+</button>
--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+	import LightSwitch from '$lib/components/light-switch.svelte';
+</script>
+
 <div class="m-4">
 	<slot />
 </div>
+
+<LightSwitch />

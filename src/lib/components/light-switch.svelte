8c02e7346787c15df165bc894c6482e95627cd0b
Jan540 <naj.schaefer@gmail.com>
2024-02-11 20:37:56 +0100

    fix light switch

--- src/lib/components/light-switch.svelte
+++ src/lib/components/light-switch.svelte
@@ -7,14 +7,12 @@
 
 <button
 	on:click={toggleMode}
-	class="fixed bottom-4 right-4 rounded-lg bg-background p-3 outline-none ring-1 ring-border transition-all hover:bg-border md:p-4"
+	class="group fixed bottom-4 right-4 rounded-lg bg-background p-3 outline-none ring-1 ring-border transition-all hover:bg-primary md:p-4"
 >
 	{#if $mode === 'dark'}
-		<Sun class="h-[1.4rem] w-[1.4rem] transition-all" />
+		<Sun class="h-[1.4rem] w-[1.4rem] transition-all group-hover:stroke-black" />
 	{:else}
-		<Moon
-			class="h-[1.4rem] w-[1.4rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-		/>
+		<Moon class="h-[1.4rem] w-[1.4rem] transition-all group-hover:stroke-white" />
 	{/if}
 	<span class="sr-only">Toggle theme</span>
 </button>

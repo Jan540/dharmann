6a8554d5efc6f239bd3ace1264d7057296804eac
Jan540 <naj.schaefer@gmail.com>
2024-02-19 14:33:49 +0100

    + analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,6 @@
+import { dev } from '$app/environment';
+import { inject as injectAnalytics } from '@vercel/analytics';
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
+injectAnalytics({ mode: dev ? 'development' : 'production' });
 injectSpeedInsights();
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 		href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💦</text></svg>"
 	/>
 	<title>Dharmann drinking water</title>
+	<meta property="og:description" content="Dharmann drinking water" />
 </svelte:head>
 
 <img
